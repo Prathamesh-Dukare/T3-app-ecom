6fbab147c3d56f80b1789500feffdc3206a342d0
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-04-10 12:40:17 +0530

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      screens: {
+        "2xl": { max: "1535px" },
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "767px" },
+        sm: { max: "639px" },
+        mobile: { max: "540px" },
+      },
     },
   },
   plugins: [],
